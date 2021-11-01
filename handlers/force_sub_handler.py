570c3aab55f2144da526fb15c92701c32ba0af6c
Mr.Rohul <92625688+Rohul25@users.noreply.github.com>
2021-11-01 16:11:33 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -47,7 +47,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Admin](https://t.me/ThaniOruvan25).",
+            text="Something went Wrong. Contact my [Support TO RᴏBᴏᴛs](https://t.me/ToRoBots).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
